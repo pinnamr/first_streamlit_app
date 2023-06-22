013f78e6ecd278f3bd284a892b9f968314d4b19c
pinnamr <137360755+pinnamr@users.noreply.github.com>
2023-06-21 21:24:37 -0400

    Update streamlit_app.py
    Added List

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ streamlit.header('Om Sai Ram')
 streamlit.text('Om Sai Ram')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
