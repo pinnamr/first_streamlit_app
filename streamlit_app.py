99227cb0b9814c1e80a01aff9ab1eeb3f9f8c4e7
pinnamr <137360755+pinnamr@users.noreply.github.com>
2023-06-21 21:16:15 -0400

    Update streamlit_app.py
    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-streamlit.header('Fruits List')
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+import streamlit
+streamlit.title('Om Sai Ram')
+streamlit.header('Sri Ram')
+streamlit.header('Om Sai Ram')
+streamlit.text('Om Sai Ram')
