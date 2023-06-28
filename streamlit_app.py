cd9514c2d03de38c188699af2664c7bd041be850
pinnamr <137360755+pinnamr@users.noreply.github.com>
2023-06-27 21:22:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy DInner')
 streamlit.title('Break Fast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, SPinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
